47694358269e59892a449339c9cf5ca1aa1bfd5d
TKB789 <toriblanche@gmail.com>
2026-04-24 07:32:25 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // IMPORTANT: change this to match your GitHub repo name.
 // If your site is https://username.github.io/century-compare/ then REPO_NAME = "century-compare"
-const REPO_NAME = "century-compare";
+const REPO_NAME = "Century-Compare";
 
 export default defineConfig({
   plugins: [react()],
