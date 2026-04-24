0c656213d3900112c38171878f3ec34138161893
TKB789 <toriblanche@gmail.com>
2026-04-24 07:21:36 -0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import CenturyCompare from './CenturyCompare.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CenturyCompare from "./CenturyCompare.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CenturyCompare />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
